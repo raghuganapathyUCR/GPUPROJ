ab5fffabf3f1ce97e3d7a4575f481e51479eada9
Raghu Ganapathy <rgana001@ucr.edu>
2023-12-12 00:20:50 -0800

    feat: parallizing the propagatenet

--- neural_net_app_kernel.cu
+++ neural_net_app_kernel.cu
@@ -2,32 +2,37 @@
 #include "neural_net_app_kernel.h"
 #include "neural_net_constants.h"
 
-
-__global__ void normalizeSunspotsKernel(REAL *sunspots, REAL min, REAL max, int size) {
+__global__ void normalizeSunspotsKernel(REAL *sunspots, REAL min, REAL max, int size)
+{
     // Kernel-level printf is supported in CUDA
-    if (threadIdx.x == 0) { // Print only once, not for every thread
+    if (threadIdx.x == 0)
+    { // Print only once, not for every thread
         printf("normalizeSunspotsKernel\n");
     }
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < size) {
+    if (idx < size)
+    {
         sunspots[idx] = ((sunspots[idx] - min) / (max - min)) * (HI - LO) + LO;
     }
 }
 
-__global__ void PropagateLayerKernel(REAL* lowerOutput, REAL* upperOutput, REAL* weight, int lowerUnits, int upperUnits, REAL gain) {
+__global__ void PropagateLayerKernel(REAL *lowerOutput, REAL *upperOutput, REAL *weight, int lowerUnits, int upperUnits, REAL gain)
+{
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < upperUnits) {
+    if (i < upperUnits)
+    {
         REAL Sum = 0;
-        for (int j = 0; j < lowerUnits; j++) {
+        for (int j = 0; j < lowerUnits; j++)
+        {
             Sum += weight[i * lowerUnits + j] * lowerOutput[j];
         }
         upperOutput[i] = 1 / (1 + exp(-gain * Sum));
     }
 }
 
-
 // Make sure to pass the device pointer as a parameter
-void normalizeSunspotsLaunch(REAL *d_sunspots, REAL min, REAL max, int size) {
+void normalizeSunspotsLaunch(REAL *d_sunspots, REAL min, REAL max, int size)
+{
     int blockSize = 256;
     int numBlocks = (size + blockSize - 1) / blockSize;
     // Call the kernel with the device pointer
@@ -35,7 +40,8 @@ void normalizeSunspotsLaunch(REAL *d_sunspots, REAL min, REAL max, int size) {
     // Always check for kernel launch error
 }
 
-void PropagateLayerLaunch(REAL* LowerOutput, REAL* UpperOutput, REAL** Weight, int LowerUnits, int UpperUnits) {
+void PropagateLayerLaunch(REAL *LowerOutput, REAL *UpperOutput, REAL **Weight, int LowerUnits, int UpperUnits)
+{
     int blockSize = 256; // Example block size, adjust based on your needs
     int numBlocks = (upperUnits + blockSize - 1) / blockSize;
 
--- neural_net_app_kernel.h
+++ neural_net_app_kernel.h
@@ -6,11 +6,11 @@
 // Declaration of the kernel function that will be implemented in .cu file
 __global__ void normalizeSunspotsKernel(REAL *sunspots, REAL min, REAL max, int size);
 
-__global__ void PropagateLayerKernel(REAL* LowerOutput, REAL* UpperOutput, REAL** Weight, int LowerUnits, int UpperUnits)
+__global__ void PropagateLayerKernel(REAL* LowerOutput, REAL* UpperOutput, REAL** Weight, int LowerUnits, int UpperUnits);
 
 // Declaration of the function that launches the kernel
 void normalizeSunspotsLaunch(REAL *d_sunspots, REAL min, REAL max, int size);
 
-void PropagateLayerLaunch(REAL* LowerOutput, REAL* UpperOutput, REAL** Weight, int LowerUnits, int UpperUnits) 
+void PropagateLayerLaunch(REAL* LowerOutput, REAL* UpperOutput, REAL** Weight, int LowerUnits, int UpperUnits);
 
 #endif // NEURAL_NET_KERNEL_H
