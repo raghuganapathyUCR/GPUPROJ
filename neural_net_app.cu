44efdeedb674da3dc668ab320e17139dbb74fbe8
Raghu Ganapathy <rgana001@ucr.edu>
2023-12-11 21:52:17 -0800

    chore: externs device pointer

--- neural_net_app.cu
+++ neural_net_app.cu
@@ -1,18 +1,26 @@
 #include <stdio.h>
+extern REAL *d_sunspots; 
 
 __global__ void normalizeSunspotsKernel(REAL *sunspots, REAL min, REAL max, int size) {
-    // put a print to check if this is being called
-    printf("normalizeSunspotsKernel\n");
+    // Kernel-level printf is supported in CUDA
+    if (threadIdx.x == 0) { // Print only once, not for every thread
+        printf("normalizeSunspotsKernel\n");
+    }
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < size) {
         sunspots[idx] = ((sunspots[idx] - min) / (max - min)) * (HI - LO) + LO;
     }
 }
 
-
-void normalizeSunspots(REAL *sunspots, REAL min, REAL max, int size) {
-    int blockSize = 256;  
+// Make sure to pass the device pointer as a parameter
+void normalizeSunspots(REAL *d_sunspots, REAL min, REAL max, int size) {
+    int blockSize = 256;
     int numBlocks = (size + blockSize - 1) / blockSize;
+    // Call the kernel with the device pointer
     normalizeSunspotsKernel<<<numBlocks, blockSize>>>(d_sunspots, min, max, size);
-
+    // Always check for kernel launch errors
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        printf("CUDA error: %s\n", cudaGetErrorString(err));
+    }
 }
